491baf3050da742b593024dadd23081a5e703f20
Adam Wynne <adamwynne@gmail.com>
2012-03-27 16:55:50 +0100

    updated to the latest ring

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
-(defproject org.clojars.hozumi/mongodb-session "1.0.1"
+(defproject org.clojars.adamwynne/mongodb-session "1.0.2"
   :description "mongodb as http session storage"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [ring/ring-core "0.3.3"]
-		 [congomongo "0.1.3-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-		     [lein-clojars "0.6.0"]])
+		 [ring/ring-core "1.0.2"]
+		 [congomongo "0.1.7"]])
