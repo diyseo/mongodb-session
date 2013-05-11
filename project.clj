9cbfc327490165493b541997c5f1af975fd90eb5
adamwynne <adamwynne@gmail.com>
2013-05-11 16:46:35 +0100

    upgraded the congo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.adamwynne/mongodb-session "1.0.2"
+(defproject org.clojars.adamwynne/mongodb-session "1.0.3"
   :description "mongodb as http session storage"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [ring/ring-core "1.0.2"]
-		 [congomongo "0.1.7"]])
+		 [congomongo "0.4.1"]])
